9a6531842b805235e1f78b837064fb7dbbed228c
Christopher A White <spacegraym3@users.noreply.github.com>
2022-09-03 13:50:52 -0700

    Added eslint

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import GlobalStyles from "./styles";
-import Pages from "./pages";
-import { ApolloProvider, ApolloClient, InMemoryCache } from "@apollo/client";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { ApolloProvider, ApolloClient, InMemoryCache } from '@apollo/client';
+import GlobalStyles from './styles';
+import Pages from './pages';
 
 const client = new ApolloClient({
-  uri: "https://server-catstronauts.herokuapp.com/", // change to YOUR own production server
+  uri: 'https://server-catstronauts.herokuapp.com/', // change this to YOUR server URI
   cache: new InMemoryCache(),
-  name: "web",
-  version: "1.0",
+  name: 'web',
+  version: '1.0',
 });
 
 ReactDOM.render(
@@ -16,5 +16,5 @@ ReactDOM.render(
     <GlobalStyles />
     <Pages />
   </ApolloProvider>,
-  document.getElementById("root")
+  document.getElementById('root'),
 );
